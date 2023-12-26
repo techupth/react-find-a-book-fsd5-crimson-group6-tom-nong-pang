9327de5767166653199060edea71b81f76c1dd4d
pang <pangton28.p@gmail.com>
2023-12-26 16:21:41 +0700

    feat(exercise): add complete exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-
+import axios from "axios";
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [textSearch, setTextSearch] = useState("");
+  const [data, setData] = useState([]);
+
+  const getTextSearch = async () => {
+    const result = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${textSearch}`
+    );
+    setData(result.data.items);
+  };
+  console.log(data);
+
+  useEffect(() => {
+    getTextSearch();
+  }, [textSearch]);
+
+  return (
+    <div>
+      <h1>Find a Book</h1>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => {
+            setTextSearch(e.target.value);
+          }}
+        />
+      </div>
+      {data.map((item, index) => (
+        <div key={index}>
+          <ul>
+            <li>{item.volumeInfo.title}</li>
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
